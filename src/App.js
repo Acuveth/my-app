c3db7204d6023e8d579959aab59c83450c65b0a5
Acuveth <lukagaberscek3@gmail.com>
2024-11-04 13:17:56 +0100

    asdasdas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import Navbar from './Navbar/Navbar';
 import LandingPage from './LandingPage/LandingPage';
 import Partners from './LandingPage/Partners';
-import Navbar from './Navbar/Navbar';
-import LandingPage from './LandingPage/LandingPage';
-import Partners from './LandingPage/Partners';
 import './App.css';
 import Features from './LandingPage/Features';
 import Pricing from './LandingPage/Pricing';
 import { BrowserRouter } from 'react-router-dom';
 import FeaturesScreen from './LandingPage/FeaturesScreen';
+import Footer from './LandingPage/Footer';
 
 function App() {
   return (
@@ -20,6 +18,7 @@ function App() {
     <Features/>
     <FeaturesScreen />
     <Pricing/>
+    <Footer />
        </BrowserRouter>
  
  
